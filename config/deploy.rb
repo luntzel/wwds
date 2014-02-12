463458ce14929e35838f22672401c0d68582e281
Mark Luntzel <mark@luntzel.com>
2014-02-11 16:49:29 -0800

    no symlinking of stuff that aint ever gonna be there ktnx

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@ set :application, "wwds"
 set :repository,  "git@github.com:luntzel/wwds.git"
 set :deploy_to, "/srv/www/wwds"
 set :shared_children, %w()
+set :normalize_asset_timestamps, false
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
